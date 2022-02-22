2283a2269dc099e8224ef722df29c387b2a0b2f4
zaki0245 <muhammad.zaki0245@gmail.com>
2022-02-22 20:39:56 +0700

    MI2F_19_MuhammadZaki

---
+++ lib/buildinlayoutwidget/decoration.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ImageTransformSample());
+}
+
+class ImageTransformSample extends StatelessWidget {
+  const ImageTransformSample({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: const Text("Contoh Transform")),
+          body: Container(
+            decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 8,
+              ),
+            ),
+            height: 200,
+            width: 300,
+            margin: const EdgeInsets.all(20),
+          )),
+    );
+  }
+}
---
+++ lib/buildinwidget/text_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text(
+        'This is Text Widget',
+      ),
+    );
+  }
+}
